5fc53d360fa0813d1065eccb94f9189b8ace5f81
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-03-07 20:28:13 +0530

    Commit

--- triplet_sum.cpp
+++ triplet_sum.cpp
@@ -7,20 +7,12 @@ int main() {
     
     int n= sizeof(arr)/sizeof(arr[0]);
     
-    int odd_count=0;
-    int even_count=0;
-    
     for(int i=0;i<n-2;i++){
         for(int j=i+1;j<n-1;j++){
             for(int k=j+1;k<n;k++){
-                int triplet_sum=arr[i]+arr[j]+arr[k];
-                if(triplet_sum%2==0){
-                    even_count++;
-                }else{
-                    odd_count++;
-                }
+                int sum= arr[i]+arr[j]+arr[k];
+                cout<<sum<<" ";
             }
         }
     }
-    cout<<even_count<<" "<<odd_count;
 }
