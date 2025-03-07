b98a732854b1d87ebea3869f1bcf9a8a50e71b82
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-03-07 16:45:45 +0530

    Array basic

---
+++ arr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // id array 
+    int arr[]={1,2,3,4,5,6};
+    
+    int arr2[4][4]={{1,2,3,4},{4,5,6,5},{7,8,9,10},{8,10,8}};
+    
+    cout<<arr[2]<<" ";
+    cout<<arr2[0][0];
+}
