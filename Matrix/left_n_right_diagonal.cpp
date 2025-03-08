ee02d4761a0b75d52cd3c089e568124edc02e351
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-03-08 18:02:01 +0530

    Printing Left and right diagonal

--- Matrix/left_n_right_diagonal.cpp
+++ Matrix/left_n_right_diagonal.cpp
@@ -3,15 +3,26 @@
 using namespace std;
 
 int main() {
-    int arr[3][4]={{1,2,3,4},{5,6,7,8},{6,7,8,9}};
+    int arr[3][3]= {{1,2,3},{4,5,6},{7,8,9}};
     int row= sizeof(arr)/sizeof(arr[0]);
     int col= sizeof(arr[0])/sizeof(arr[0][0]);
     
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
-            if(i==0 || j==0 || i==row-1 || j==col-1){
+            if(i==j){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+    
+    cout<<endl;
+    
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(i+j==row-1 && i!=j){
                 cout<<arr[i][j]<<" ";
             }
         }
     }
 }
+
